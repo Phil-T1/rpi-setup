3be0e3b975563bb40a997cbfabb3b905842d0e0a
Phil-T1 <88270060+Phil-T1@users.noreply.github.com>
2023-05-03 12:57:58 +0100

    Delete wol.sh

--- wol.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#Call wakeonlan
-wakeonlan C8:F7:50:70:0A:78
-#wakeonlan 88:D7:F6:9F:66:93
-
-#Notify user
-echo "Subject: Computers woken!" |msmtp --debug --from=default -t phil.tegg@gmail.com
